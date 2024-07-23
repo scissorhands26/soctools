2e9d48e8672b181360bbe94788eb2e1fe870dfec
ScissorHands26 <ryan.scissons@gmail.com>
2024-07-23 09:06:05 -0400

    Updated app

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,25 +8,25 @@ export default function NavBar() {
   return (
     <header className="bg-slate-950 text-white py-6 px-4 md:px-6">
       <div className="container flex items-center justify-between">
-        <Link href="/" className="flex items-center gap-2" prefetch={false}>
+        <Link href="#" className="flex items-center gap-2" prefetch={false}>
           <ShieldIcon className="w-6 h-6" />
           <span className="text-xl font-bold">SOC Tools</span>
         </Link>
         <nav className="hidden md:flex items-center gap-4">
-          <Link
+          {/* <Link
             href="#"
             className="text-sm font-medium hover:underline"
             prefetch={false}
           >
             Features
-          </Link>
-          <Link
+          </Link> */}
+          {/* <Link
             href="apps"
             className="text-sm font-medium hover:underline"
             prefetch={false}
           >
             Apps
-          </Link>
+          </Link> */}
           <Link
             href="#"
             className="text-sm font-medium hover:underline"
@@ -35,11 +35,11 @@ export default function NavBar() {
             Resources
           </Link>
           <Link
-            href="#"
-            className="inline-flex h-9 items-center justify-center rounded-md bg-primary-foreground px-4 py-2 text-sm font-medium text-primary shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            href="apps"
+            className="inline-flex h-9 items-center justify-center rounded-md bg-primary-foreground px-4 py-2 text-sm font-medium text-primary shadow transition-colors hover:bg-slate-950 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
             prefetch={false}
           >
-            Get Started
+            Apps
           </Link>
         </nav>
         <Button variant="ghost" size="icon" className="md:hidden">
