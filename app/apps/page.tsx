6767e35420a3baaffd290f369827d029b216c131
ScissorHands26 <ryan.scissons@gmail.com>
2024-07-23 15:46:11 -0400

    Updated app

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -31,7 +31,7 @@ export default function AppsPage() {
 
   return (
     <div className="h-screen bg-black">
-      <h1 className="text-4xl font-bold text-center mb-4">Apps</h1>
+      <h1 className="text-4xl font-bold text-center mb-4 py-8">Apps</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 container mx-auto">
         {apps.map((app) => (
           <Card key={app.href}>
