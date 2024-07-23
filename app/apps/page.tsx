a4d0b5655d2a7b23237630eb6fde75695fc54315
ScissorHands26 <ryan.scissons@gmail.com>
2024-07-23 15:38:43 -0400

    Updated app

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -30,7 +30,7 @@ export default function AppsPage() {
   ];
 
   return (
-    <div className="container mx-auto h-screen">
+    <div className="container mx-auto h-screen bg-black">
       <h1 className="text-4xl font-bold text-center mb-4">Apps</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {apps.map((app) => (
