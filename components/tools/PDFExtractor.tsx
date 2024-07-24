57a5b3547fbe322dfbef231835b7221d37e9f91e
ScissorHands26 <ryan.scissons@gmail.com>
2024-07-24 12:30:15 -0400

    Updated app

--- components/tools/PDFExtractor.tsx
+++ components/tools/PDFExtractor.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import { UploadIcon } from "lucide-react";
-import { useState, ChangeEvent, DragEvent } from "react";
-// @ts-ignore
-import * as pdfjsLib from "pdfjs-dist/webpack";
+import { useState, ChangeEvent } from "react";
+import { getDocument, GlobalWorkerOptions } from "pdfjs-dist/legacy/build/pdf";
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdfjs/pdf.worker.js";
+GlobalWorkerOptions.workerSrc = "/pdfjs/pdf.worker.js";
 
 interface PDFMetadata {
   PDFFormatVersion?: string;
   Language?: string;
-  EncryptFilterName?: string;
-  IsLinearized?: string;
-  IsAcroFormPresent?: string;
-  IsXFAPresent?: string;
-  IsCollectionPresent?: string;
-  IsSignaturesPresent?: string;
   Author?: string;
   Creator?: string;
   CreationDate?: string;
@@ -26,15 +18,6 @@ interface PDFMetadata {
 export default function PDFMetadataExtractor() {
   const [file, setFile] = useState<File | null>(null);
   const [pdfMetadata, setPDFMetadata] = useState<PDFMetadata | null>(null);
-
-  const handleDrop = (event: DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    const droppedFile = event.dataTransfer.files[0];
-    if (droppedFile) {
-      setFile(droppedFile);
-      extractPDFMetadata(droppedFile);
-    }
-  };
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
@@ -49,35 +32,17 @@ export default function PDFMetadataExtractor() {
     }
   };
 
-  const convertTimestamp = (timestamp: string | undefined): string => {
-    if (!timestamp) {
-      return "";
-    }
-    const year = timestamp.substring(2, 6);
-    const month = timestamp.substring(6, 8);
-    const day = timestamp.substring(8, 10);
-    const hours = timestamp.substring(10, 12);
-    const minutes = timestamp.substring(12, 14);
-    const seconds = timestamp.substring(14, 16);
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-  };
-
   const extractPDFMetadata = async (file: File) => {
     const reader = new FileReader();
     reader.onload = async () => {
       const typedArray = new Uint8Array(reader.result as ArrayBuffer);
       try {
-        const pdf = await pdfjsLib.getDocument(typedArray).promise;
+        const loadingTask = getDocument({ data: typedArray });
+        const pdf = await loadingTask.promise;
         const { info } = await pdf.getMetadata();
         setPDFMetadata({
           PDFFormatVersion: info.PDFFormatVersion,
           Language: info.Language,
-          EncryptFilterName: info.EncryptFilterName,
-          IsLinearized: info.IsLinearized,
-          IsAcroFormPresent: info.IsAcroFormPresent,
-          IsXFAPresent: info.IsXFAPresent,
-          IsCollectionPresent: info.IsCollectionPresent,
-          IsSignaturesPresent: info.IsSignaturesPresent,
           Author: info.Author,
           Creator: info.Creator,
           CreationDate: info.CreationDate,
@@ -93,11 +58,7 @@ export default function PDFMetadataExtractor() {
 
   return (
     <div className="flex flex-col items-center justify-center w-full">
-      <div
-        onDragOver={(event) => event.preventDefault()}
-        onDrop={handleDrop}
-        className="w-full bg-slate-950 rounded-lg p-4 shadow-md border border-slate-900 flex flex-col items-center justify-center"
-      >
+      <div className="w-full bg-slate-950 rounded-lg p-4 shadow-md border border-slate-900 flex flex-col items-center justify-center">
         <input
           accept=".pdf"
           type="file"
@@ -126,44 +87,6 @@ export default function PDFMetadataExtractor() {
                   <p className="text-white">{pdfMetadata?.Language}</p>
                 </div>
                 <div>
-                  <p className="text-gray-300 font-medium">
-                    Encrypt Filter Name:
-                  </p>
-                  <p className="text-white">{pdfMetadata?.EncryptFilterName}</p>
-                </div>
-                <div>
-                  <p className="text-gray-300 font-medium">Is Linearized:</p>
-                  <p className="text-white">{pdfMetadata?.IsLinearized}</p>
-                </div>
-
-                <div>
-                  <p className="text-gray-300 font-medium">
-                    Is AcroForm Present:
-                  </p>
-                  <p className="text-white">{pdfMetadata?.IsAcroFormPresent}</p>
-                </div>
-
-                <div>
-                  <p className="text-gray-300 font-medium">Is XFA Present:</p>
-                  <p className="text-white">{pdfMetadata?.IsXFAPresent}</p>
-                </div>
-                <div>
-                  <p className="text-gray-300 font-medium">
-                    Is Collection Present:
-                  </p>
-                  <p className="text-white">
-                    {pdfMetadata?.IsCollectionPresent}
-                  </p>
-                </div>
-                <div>
-                  <p className="text-gray-300 font-medium">
-                    Is Signatures Present:
-                  </p>
-                  <p className="text-white">
-                    {pdfMetadata?.IsSignaturesPresent}
-                  </p>
-                </div>
-                <div>
                   <p className="text-gray-300 font-medium">Author:</p>
                   <p className="text-white">{pdfMetadata?.Author}</p>
                 </div>
@@ -173,17 +96,13 @@ export default function PDFMetadataExtractor() {
                 </div>
                 <div>
                   <p className="text-gray-300 font-medium">Creation Date:</p>
-                  <p className="text-white">
-                    {convertTimestamp(pdfMetadata?.CreationDate)}
-                  </p>
+                  <p className="text-white">{pdfMetadata?.CreationDate}</p>
                 </div>
                 <div>
                   <p className="text-gray-300 font-medium">
                     Modification Date:
                   </p>
-                  <p className="text-white">
-                    {convertTimestamp(pdfMetadata?.ModDate)}
-                  </p>
+                  <p className="text-white">{pdfMetadata?.ModDate}</p>
                 </div>
                 <div>
                   <p className="text-gray-300 font-medium">Producer:</p>
@@ -193,9 +112,8 @@ export default function PDFMetadataExtractor() {
             </div>
           ) : (
             <div className="flex flex-col items-center justify-center">
-              <UploadIcon className="h-12 w-12 text-gray-400 mb-4" />
               <p className="text-gray-300 font-medium text-center">
-                Drag and drop a file here or click to browse
+                Click to select a PDF file
               </p>
             </div>
           )}
