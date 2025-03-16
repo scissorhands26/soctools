d42552585faccaf5c74d934ee831d7e9360a9d78
ScissorHands26 <ryan.scissons@gmail.com>
2025-03-16 14:00:04 -0400

    Added analytics

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
 
